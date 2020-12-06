3594bb031bf4d6cd8866dba4e9f2f5fba6714cd9
Hannah-Scheuer <hscheuer20@gmail.com>
2020-12-06 14:11:06 -0500

    I think I made the constructor, but I'm not sure it does the right thing

--- Range.java
+++ Range.java
@@ -2,7 +2,8 @@ import java.util.NoSuchElementException;
 public class Range implements IntegerSequence{
   private int start,end,current;
   public Range(int start, int end){
-    
+    start = this.start;
+    end = this.end;
   }
 
 }
