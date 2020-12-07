fe573420b8d133f561c785f48044ff95a01d6fe7
Hannah-Scheuer <hscheuer20@gmail.com>
2020-12-07 12:55:35 -0500

    fixed next()

--- ArraySequence.java
+++ ArraySequence.java
@@ -1,10 +1,11 @@
 import java.util.NoSuchElementException;
 public class ArraySequence implements IntegerSequence{
   private int currentIndex;
-  private int []data;
+  private int [] data;
 
   /*Construct the sequence by copying values from the other array into the data array*/
   public ArraySequence(int [] other){
+    int [] data = new int[other.length];
     for (int i=0;i<other.length;i++){
       data[i]=other[i];
     }
@@ -17,11 +18,19 @@ public class ArraySequence implements IntegerSequence{
 
   public int next(){
     if (!hasNext()) {
-      throw new NoSuchElementException("The number " +current+1+ " is not within the bounds of this Range");
+      throw new NoSuchElementException("The number " +currentIndex+1+ " is not within the bounds of this Range");
     }
-    int curr = currentIndex;
+    int curr = data[currentIndex];
     currentIndex += 1;
     return curr;
+  }
+
+  public int length(){
+    return data.length;
+  }
+
+  public void reset(){
+    currentIndex=0;
   }
 
 
