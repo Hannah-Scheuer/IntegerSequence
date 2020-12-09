85a720e4cef2e170b46b36a2b6770a4970813bc1
Hannah-Scheuer <hscheuer20@gmail.com>
2020-12-08 22:21:48 -0500

    started the second constructor

--- ArraySequence.java
+++ ArraySequence.java
@@ -1,15 +1,27 @@
 import java.util.NoSuchElementException;
 public class ArraySequence implements IntegerSequence{
   private int currentIndex;
-  private int [] data;
+  private int[] data;
 
   /*Construct the sequence by copying values from the other array into the data array*/
   public ArraySequence(int [] other){
-    int [] data = new int[other.length];
+    int[] data = new int[other.length];
     for (int i=0;i<other.length;i++){
       data[i]=other[i];
     }
     currentIndex=0;
+  }
+
+  public ArraySequence(IntegerSequence otherseq){
+    otherseq.reset();
+    int[] data = new int[otherseq.length()];
+    int temp = 1;
+    while (otherseq.hasNext()){
+      data[temp] = otherseq.next();
+      temp += 1;
+    }
+    otherseq.reset();
+    currentIndex = 0;
   }
 
   public boolean hasNext(){
